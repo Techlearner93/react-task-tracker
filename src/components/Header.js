b1b1df52c62a2e8fff5d19d20b5c30338a1858b4
Royce Er <royceforlife@gmail.com>
2022-05-17 22:51:08 +0800

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function Header() {
+export default function Header({ title }) {
+
     return (
         <header>
-            <h1>Task Tracker</h1>
+            <h1>{title}</h1>
         </header>
     )
+}
+
+Header.defaultProps = {
+    title: "Default Props Task Tracker"
+}
+
+// propTypes allows you to define what data type you want for the props passed
+// .isRequired makes it mandatory for a prop to be passed, else an error will be thrown
+Header.propTypes = {
+    title: PropTypes.string.isRequired
 }
